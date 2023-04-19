871804875a4e26f672b83e9b2cd465f061653e3a
Emiliano Jiménez <emilianokaleb@gmail.com>
2023-04-19 16:37:37 -0600

    errores en videojuegos

---
+++ js/index.js
@@ -0,0 +1,34 @@
+function iniciar(){
+    document.querySelector("#hamburger").addEventListener('click', 
+hamburger, false);
+
+    document.querySelector("#bubble-1").addEventListener('click',
+    changeBubble, false);
+}
+
+window.addEventListener('load', iniciar, false);
+
+function drop(){
+    
+    
+}
+
+function hamburger(){
+    var menu = document.querySelector("#hamburger");
+    var navbar = document.querySelector(".navbar");
+    var nav = document.querySelector(".nav");
+    
+    menu.classList.toggle("rotate180");
+    navbar.classList.toggle("droppable");
+    nav.classList.toggle("appear");
+    //nav.classList.toggle("fadeable");
+}
+
+function changeBubble(){
+    var textos = document.querySelectorAll(".bubble__text");
+    
+    for(var i = 0; i < textos.length; i++){
+        textos[i].classList.toggle("appear");
+        textos[i].classList.toggle("drop");
+    }
+}
